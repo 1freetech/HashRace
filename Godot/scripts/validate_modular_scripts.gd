024762f00eb2db8aa4dd04f357fedc63494b359a
1freetech <matthanburton@gmail.com>
2026-09-20 14:30:05 -0700

    Validate v0.129 gameplay layer

--- Godot/scripts/validate_modular_scripts.gd
+++ Godot/scripts/validate_modular_scripts.gd
@@ -62,6 +62,7 @@ const SCRIPTS: Array[String] = [
     "res://scripts/world_v126.gd",
     "res://scripts/world_v127.gd",
     "res://scripts/world_v128.gd",
+    "res://scripts/world_v129.gd",
 ]
 
 func _initialize() -> void:
