a5e6cf9a0ab72adf013bbf476a7608abb9166d49
1freetech <matthanburton@gmail.com>
2026-09-21 04:05:43 -0700

    Parse current Hash Race world layers in CI

--- Godot/scripts/validate_modular_scripts.gd
+++ Godot/scripts/validate_modular_scripts.gd
@@ -34,7 +34,7 @@ const SCRIPTS: Array[String] = [
     "res://scripts/utility_props_catalog.gd",
     "res://scripts/wind_turbine_catalog.gd",
     "res://scripts/asic_air_s19j_catalog.gd",
-        "res://scripts/electrical_distribution_catalog.gd",
+    "res://scripts/electrical_distribution_catalog.gd",
     "res://scripts/world_v087.gd",
     "res://scripts/world_scale_rules.gd",
     "res://scripts/camera_proportion_controller.gd",
@@ -63,7 +63,12 @@ const SCRIPTS: Array[String] = [
     "res://scripts/world_v126.gd",
     "res://scripts/world_v127.gd",
     "res://scripts/world_v128.gd",
-        "res://scripts/world_v129.gd",
+    "res://scripts/world_v129.gd",
+    "res://scripts/world_v130.gd",
+    "res://scripts/world_v131.gd",
+    "res://scripts/world_v132.gd",
+    "res://scripts/world_v133.gd",
+    "res://scripts/world_v134.gd",
 ]
 
 func _initialize() -> void:
