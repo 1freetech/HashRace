973b15e99618b9097851321e98f053fef2796941
1freetech <matthanburton@gmail.com>
2026-09-22 23:07:34 -0700

    v0.157: validate live visual-reset layer

--- Godot/scripts/validate_modular_scripts.gd
+++ Godot/scripts/validate_modular_scripts.gd
@@ -83,6 +83,7 @@ const SCRIPTS: Array[String] = [
     "res://scripts/world_v154.gd",
     "res://scripts/world_v155.gd",
     "res://scripts/world_v156.gd",
+    "res://scripts/world_v157.gd",
 ]
 
 func _initialize() -> void:
